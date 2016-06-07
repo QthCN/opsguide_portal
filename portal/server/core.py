1bcfe52548de94c2cc29b2e81e28c562368d9564
TianHuan <tianhuan@bingotree.cn>
2016-06-07 16:06:27 +0800

    add 500.html

--- portal/server/core.py
+++ portal/server/core.py
@@ -5,7 +5,7 @@ import os
 import eventlet
 from eventlet import wsgi
 from oslo_config import cfg
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, Blueprint
 import pbr.version
 
 from portal import config
@@ -14,11 +14,26 @@ from portal.portal.core import portal_page
 
 CONF = cfg.CONF
 
+common_page = Blueprint('common_page', __name__,
+                        template_folder='../templates')
+
+
+@common_page.route('/404.html', methods=['GET'])
+def show_404():
+    return render_template('404.html', resource_class='404')
+
+
+@common_page.route('/500.html', methods=['GET'])
+def show_500():
+    return render_template('500.html', resource_class='500')
+
+
 _APP = None
 
 MODULES = [
     (portal_page, ''),
-    (login_page, '')
+    (login_page, ''),
+    (common_page, '')
 ]
 
 
@@ -92,6 +107,11 @@ def before_run(possible_topdir, conf_dir="etc",
     @app.errorhandler(404)
     def page_not_found(error):
         return render_template('404.html'), 404
+
+    # set 500 page
+    @app.errorhandler(500)
+    def page_not_found(error):
+        return render_template('500.html'), 500
 
     return app
 
